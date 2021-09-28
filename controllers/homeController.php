3d9fff3d289757b4361f7cc67117958fdcb5d92f
elouan <elouan.peurou@gmail.com>
2021-09-28 19:52:39 +0200

    codacy test

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -5,16 +5,16 @@ class homeController extends controller {
     public function homepage(){
         global $twig,$_SESSION,$nameUser,$firstNameUser,$emailUser,$admin;
         if(isset($_SESSION['idUser'])){
-            print $this->twig->render('index.html.twig',[
+            print_r ($this->twig->render('index.html.twig',[
                 'sessionToken' => $_SESSION['token'],
                 'session' => $_SESSION['idUser'],
                 'nameUser' => $nameUser,
                 'firstName' => $firstNameUser,
                 'email' => $emailUser,
                 'admin' => $admin
-            ]);
+            ]));
         } else {
-            echo $this->twig->render('index.html.twig');
+            print_r ($this->twig->render('index.html.twig'));
         }
     }
         

9d6b9860325623e40604355b11b055e70a53ca47
elouan <elouan.peurou@gmail.com>
2021-09-28 11:51:11 +0200

    Update bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version OVH
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : elouanr29.mysql.db
--- Généré le : jeu. 23 sep. 2021 à 10:26
--- Version du serveur : 5.6.50-log
--- Version de PHP : 7.4.21
+-- Host: localhost:3306
+-- Generation Time: Sep 28, 2021 at 09:47 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,15 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `elouanr29`
+-- Database: `elouanr29`
 --
-CREATE DATABASE IF NOT EXISTS `elouanr29` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `elouanr29`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE `category` (
@@ -35,7 +34,7 @@ CREATE TABLE `category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `category`
+-- Dumping data for table `category`
 --
 
 INSERT INTO `category` (`id`, `name`) VALUES
@@ -45,7 +44,7 @@ INSERT INTO `category` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `comment`
+-- Table structure for table `comment`
 --
 
 CREATE TABLE `comment` (
@@ -58,20 +57,20 @@ CREATE TABLE `comment` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `comment`
+-- Dumping data for table `comment`
 --
 
 INSERT INTO `comment` (`id`, `date`, `content`, `validate`, `idUser`, `idPost`) VALUES
-(2, '2021-07-05 16:14:12', 'ce post est vraiment super ', 1, 1, 4),
-(3, '2021-07-07 08:58:29', 'super contenu ', 1, 2, 4),
-(5, '2021-07-07 09:22:59', 'ce post est franchement pas terrible', 1, 2, 44),
-(48, '2021-09-02 09:42:59', 'TROP BIEN!!', 0, 1, 4),
-(49, '2021-09-02 09:43:42', 'sympa ce post', 0, 1, 4);
+(2, '2021-07-05 16:14:12', 'ce post est vraiment super ', 1, 18, 4),
+(3, '2021-07-07 08:58:29', 'super contenu ', 1, 18, 44),
+(5, '2021-07-07 09:22:59', 'ce post est franchement pas terrible', 1, 17, 44),
+(48, '2021-09-02 09:42:59', 'TROP BIEN!!', 0, 18, 44),
+(49, '2021-09-02 09:43:42', 'sympa ce post', 0, 18, 4);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `post`
+-- Table structure for table `post`
 --
 
 CREATE TABLE `post` (
@@ -86,17 +85,17 @@ CREATE TABLE `post` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `post`
+-- Dumping data for table `post`
 --
 
 INSERT INTO `post` (`id`, `title`, `content`, `description`, `date`, `idUser`, `idCategory`, `author`) VALUES
-(4, 'Nouveau blog!', 'Le blog à été conçu pour être agréable à l\'oeil et à l\'usage. Il vous plait?', 'Bienvenue sur le blog!', '2021-09-22 15:44:15', 1, 2, 'Elouan'),
-(44, 'PHP c\'est vraiment bien!', 'PHP est un bon language pour apprendre.', 'Blabla à propos de php', '2021-09-22 15:54:11', 1, 2, 'Elouan');
+(4, 'Nouveau blog!', 'Le blog à été conçu pour être agréable à l\'oeil et à l\'usage. Il vous plait?', 'Bienvenue sur le blog!', '2021-09-22 15:44:15', 17, 2, 'Elouan'),
+(44, 'PHP c\'est vraiment bien!', 'PHP est un bon language pour apprendre.', 'Blabla à propos de php', '2021-09-22 15:54:11', 17, 2, 'Elouan');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -107,30 +106,30 @@ CREATE TABLE `user` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `pseudo` varchar(255) NOT NULL,
-  `admin` tinyint(1) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
   `creationDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `firstName`, `name`, `civility`, `email`, `password`, `pseudo`, `admin`, `creationDate`) VALUES
-(1, 'elouan', 'peurou', 'm', 'elouan@npack.fr', '******', 'tim', 1, '2021-07-01 13:32:29'),
-(2, 'Tom', 'Dupont', 'm', 'tomDupont@gmail.com', '**', 'tom18', 0, '2021-07-02 14:20:42');
+(17, 'peurou', 'elouan', 'M', 'admin@gmail.com', '$2y$10$FJhobVR9kWnGbxycNcP4CetzBrRBT74T447UY86PVoEB5Obm5.Bui', 'admin', 1, '2021-09-28 11:33:52'),
+(18, 'Dupont', 'Tom', 'M', 'nonAdmin@gmail.com', '$2y$10$eRwslhWDfO1yFeT4/RxEMe2Z9TXRp.6dC93rLcM9V5egyAnYGz5pK', 'nonAdmin', 0, '2021-09-28 11:39:38');
 
 --
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `category`
+-- Indexes for table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `comment`
+-- Indexes for table `comment`
 --
 ALTER TABLE `comment`
   ADD PRIMARY KEY (`id`),
@@ -138,7 +137,7 @@ ALTER TABLE `comment`
   ADD KEY `idPost` (`idPost`);
 
 --
--- Index pour la table `post`
+-- Indexes for table `post`
 --
 ALTER TABLE `post`
   ADD PRIMARY KEY (`id`),
@@ -146,52 +145,52 @@ ALTER TABLE `post`
   ADD KEY `idCategory` (`idCategory`);
 
 --
--- Index pour la table `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `category`
+-- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT pour la table `comment`
+-- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
--- AUTO_INCREMENT pour la table `post`
+-- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
--- AUTO_INCREMENT pour la table `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- Contraintes pour les tables déchargées
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `comment`
+-- Constraints for table `comment`
 --
 ALTER TABLE `comment`
   ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`idPost`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `post`
+-- Constraints for table `post`
 --
 ALTER TABLE `post`
   ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`),

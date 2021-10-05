a8da308b39ea857bf6b0d150b684a5bf35d7b663
elouan <elouan.peurou@gmail.com>
2021-10-05 15:58:26 +0200

    delete $_SESSION from login controller

--- controllers/loginRegisterController.php
+++ controllers/loginRegisterController.php
@@ -4,13 +4,13 @@
 class loginRegisterController extends controller {
 
     public function login(){
-        global $id,$_SESSION;
+        global $id;
         print_r ($this->twig->render('login.html.twig',[
         ])); 
     }
 
     public function register(){
-        global $id,$_SESSION;
+        global $id;
         print_r ($this->twig->render('register.html.twig',[
         ]));
     }
